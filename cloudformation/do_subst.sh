49059dd22c7d3f770a035cd33fe0ca78ed5821fd
Simon Hildrew <simon@hildrew.net>
2014-07-24 13:16:30 +0100

    Valid CFN and get substitution working

---
+++ cloudformation/do_subst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ev
+# carry out substitution of {$1} with the given value ($2)
+if [ -z "$1" -o -z "$2" -o -z "$3" ]
+then
+  echo "Usage: $0 file key value"
+  exit 1
+fi
+
+FILE=$1
+KEY=$2
+VALUE=$3
+
+sed -i "s/{${KEY}}/${VALUE}/g" ${FILE}
